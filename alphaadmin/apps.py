292cf4ca730361e765c128da80c937ddbd76c8a5
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-07-02 17:11:40 +0530

    created beta, betaadmin, alpha, alphaadmin

---
+++ alphaadmin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AlphaadminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'alphaadmin'
--- cscodebase/settings.py
+++ cscodebase/settings.py
@@ -26,7 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'betaadmin',
+    'alphaadmin',
+    'beta',
+    'alpha',
 ]
 
 MIDDLEWARE = [
