c8afaee37bfd638e0c0b5344eefe51243473723a
Raktimava960 <89402627+Raktimava960@users.noreply.github.com>
2024-08-26 19:41:02 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('sign-in/', views.signIn, name='sign-in'),
-    path('registration/', views.singUp, name='registration'),
+    path('registration/', views.signUp, name='registration'),
     path('forget-password/', views.forget_password, name='forget-password'),
+    path('student-dashboard/',views.student_dashboard, name='student-dashboard'),
 ]
