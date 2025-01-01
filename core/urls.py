df6346bdedffe0a04044713f4713ed26e71afff6
Munesh Kushwah <54856340+immunesh@users.noreply.github.com>
2025-01-01 14:30:40 +0530

    Delete core directory

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('sign-in/', views.signIn, name='sign-in'),
-    path('registration/', views.signUp, name='registration'),
-    path('forget-password/', views.forget_password, name='forget-password'),
-    path('student-dashboard/',views.student_dashboard, name='student-dashboard'),
-]
